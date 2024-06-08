7bc666b22dbf743edcbf337bcf044e7cc79b0d93
gurudanito001 <youareontodan@gmail.com>
2024-06-09 00:04:29 +0100

    changed structure

--- src/prisma/migrations/20240604081750_changed_datatype_of_code_in_email_model_to_int/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `code` to the `Email` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Email" DROP COLUMN "code",
-ADD COLUMN     "code" INTEGER NOT NULL;
